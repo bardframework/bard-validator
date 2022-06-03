ac2f6ca0e43ddd1d30a2e5c5c18277b148ad1db3
Vahid Zafari <va.zafari@gmail.com>
2022-06-03 10:29:24 +0430

    clean code

--- src/main/java/org/bardframework/validator/field/collection/CollectionUuidValidator.java
+++ src/main/java/org/bardframework/validator/field/collection/CollectionUuidValidator.java
@@ -6,6 +6,7 @@ import org.bardframework.validator.field.SingleFieldValidatorAbstract;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by v.zafari on 30/01/2016.
@@ -23,7 +24,7 @@ public class CollectionUuidValidator extends SingleFieldValidatorAbstract<Collec
     @Override
     protected boolean isValid(Collection<String> collection) {
         try {
-            collection.forEach(java.util.UUID::fromString);
+            collection.forEach(UUID::fromString);
             return true;
         } catch (Exception e) {
             LOGGER.debug("invalid uuid in list [{}]", collection);
